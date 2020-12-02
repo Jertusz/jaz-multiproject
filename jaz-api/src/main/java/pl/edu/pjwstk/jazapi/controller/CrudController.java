286a529fdf752db426f900468b61ca9a1b1d58fd
Jedrzej Szadejko <jendziu@gmail.com>
2020-12-02 14:57:16 +0100

    Added sorting and addtitonal description

--- jaz-api/src/main/java/pl/edu/pjwstk/jazapi/controller/CrudController.java
+++ jaz-api/src/main/java/pl/edu/pjwstk/jazapi/controller/CrudController.java
@@ -1,15 +1,13 @@
 package pl.edu.pjwstk.jazapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pjwstk.jazapi.service.CrudService;
 import pl.edu.pjwstk.jazapi.service.DbEntity;
 
-import java.awt.print.Pageable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -24,13 +22,19 @@ public abstract class CrudController<T extends DbEntity> {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Map<String, Object>>> getAll(@RequestParam(name = "size", defaultValue = "4") int size, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "sort", defaultValue = "id") String sort) {
+    public ResponseEntity<List<Map<String, Object>>> getAll(@RequestParam(name = "size", defaultValue = "4") int size, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "sort", defaultValue = "asc,id") String sort) {
         try {
-            List<T> all = service.getAll(page, size);
+            String[] sortType = sort.split(",");
+            List<T> all = service.getAll(page, size, sortType);
             List<Map<String, Object>> payload = all.stream()
                     .map(obj -> transformToDTO().apply(obj))
                     .collect(Collectors.toList());
-
+            Map<String, Object> data = new HashMap<>();
+            data.put("Page size", size);
+            data.put("Page number", page);
+            data.put("Sort direction", sortType[0]);
+            data.put("Sorting key", sortType[1]);
+            payload.add(data);
             return new ResponseEntity<>(payload, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
